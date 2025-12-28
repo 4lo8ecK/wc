d5857073f97e1868e3da9a5b93e7b3cc1b098b56
4lo8ecK <4lo8ecK@vk.com>
2025-12-28 19:34:29 +0300

    initial part of text work in C

--- main.c
+++ main.c
@@ -2,50 +2,97 @@
 #include "wc/wc.h"
 #endif
 
-#include <string.h>
+#include <windows.h>
 #include <stdio.h>
 #include <locale.h>
 
+#include <time.h>
+
 #define TEST
 #ifdef TEST
-int get_len(const unsigned char* text) {
-    int count = 0;
 
-    for(;;) {
-        char chr = text[count];
-        printf("%d - %d\n", count, chr);
-        if (chr == '\0') {
-            break;
-        }
+DWORD str_len(LPCSTR str) {
+    return MultiByteToWideChar(CP_UTF8, 0, str, -1, NULL, 0) - 1;
+}
+
+DWORD wstr_len(LPCWSTR wstr) {
+    return WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL) - 1;
+}
+
+LPCWSTR char_to_wchar(LPCSTR txt) {
+    DWORD len = str_len(txt);
+    if (len == 0) { return NULL; }
+    
+    LPWSTR wstr = (LPWSTR)malloc(len + 1);
+    MultiByteToWideChar(CP_UTF8, 0, txt, -1, wstr, len);
+    wstr[len] = L'\0';
+    return wstr;
+}
+
+LPCSTR wchar_to_char(LPCWSTR wtxt) {
+    DWORD len = wstr_len(wtxt);
+    if (len == 0) { return NULL; }
+    
+    LPSTR str = (LPSTR)malloc(len + 1);
+    WideCharToMultiByte(CP_UTF8, 0, wtxt, -1, str, len, NULL, NULL);
+    str[len] = '\0';
+    return str;
+}
+
+DWORD int_len(DWORD number) {
+    DWORD count = 0;
+    while (number != 0) {
+        number /= 10;
         count++;
     }
-
     return count;
 }
+
+LPCWSTR int_to_wchar(DWORD num) {
+    DWORD len = int_len(num);
+    LPWSTR wstr = (LPWSTR)malloc(len + 1);
+    _itow(num, wstr, 10);
+    wstr[len] = L'\0';
+    return wstr;
+}
+
+LPCSTR int_to_char(DWORD num) {
+    DWORD len = int_len(num);
+    LPSTR str = (LPSTR)malloc(len + 1);
+    itoa(num, str, 10);
+    str[len] = '\0';
+    return str;
+}
+
+
 #endif
 
 int main() {
-
-#ifdef TEST
-    setlocale(LC_ALL, ".UTF-8");
+    #ifdef TEST
+    // time_t start_time = time(NULL);
+    setlocale(LC_ALL, "");
 
     #if 1
+    // time_t str_beg_time = time(NULL);
+    LPCSTR str = "Пример текста, text example";
+    LPCWSTR wstr = L"Пример текста, text example";
+    LPCWSTR wstr1 = char_to_wchar(str);
+    DWORD num = 123416;
+    // time_t str_end_time = time(NULL);
     
-    const char* test_line_en = "Test text with len = 23";
-    int len1_en = strlen(test_line_en);
-    int len2_en = sizeof(*test_line_en);
+    // time_t print_time_beg = time(NULL);
+    for (int i = 0; i < 50; i++)
+    {
+        printf("Iteration %d\n", i);
+        printf("\t%ld\n", str_len("abcdef"));
+        printf("\t%ld\n", wstr_len(L"abcdef"));
+        printf("\t%ls\n", int_to_wchar(num));
+        printf("\t%s\n", str);
+        printf("\t%S\n", wstr);
+        printf("\t%S\n", wstr1);       
+        printf("\n");
+    }
     
-    printf("%s\n-> %d %d\n", test_line_en, len1_en, len2_en);
-    get_len(test_line_en);
-    
-    const char* test_line_ru = "Текст в длину = 18";
-    int len1_ru = strlen(test_line_ru);
-    int len2_ru = sizeof(test_line_ru[0]);
-    
-    printf("%s\n-> %d %d\n", test_line_ru, len1_ru, len2_ru);
-    
-    get_len(test_line_ru);
     #endif
-
 #endif
 }
